7c2b62dcc15421b1b85044efef3a166535093396
anT0ny54 <109664822+anT0ny54@users.noreply.github.com>
2026-09-13 22:00:43 +0700

    Implement ruleset conversion functionality

---
+++ internal/ruleset/ruleset.go
@@ -0,0 +1,91 @@
+package ruleset
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+type Options struct {
+	Converter string
+	Input     string
+	Output    string
+	Log       string
+}
+
+func Convert(ctx context.Context, o Options) error {
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	if strings.TrimSpace(o.Converter) == "" {
+		return fmt.Errorf("converter is empty")
+	}
+	if strings.TrimSpace(o.Input) == "" {
+		return fmt.Errorf("input is empty")
+	}
+	if strings.TrimSpace(o.Output) == "" {
+		return fmt.Errorf("output is empty")
+	}
+	if _, err := os.Stat(o.Input); err != nil {
+		return fmt.Errorf("input: %w", err)
+	}
+	outDir := filepath.Dir(o.Output)
+	if outDir == "" {
+		outDir = "."
+	}
+	if err := os.MkdirAll(outDir, 0755); err != nil {
+		return err
+	}
+	tmp, err := os.CreateTemp(outDir, "."+filepath.Base(o.Output)+".tmp-*")
+	if err != nil {
+		return err
+	}
+	tmpPath := tmp.Name()
+	tmp.Close()
+	os.Remove(tmpPath)
+	defer os.Remove(tmpPath)
+	args := []string{"--input_format=filter-list", "--output_format=unindexed-ruleset", "--input_files=" + o.Input, "--output_file=" + tmpPath}
+	cmd := exec.CommandContext(ctx, o.Converter, args...)
+	var logf *os.File
+	if o.Log != "" {
+		logDir := filepath.Dir(o.Log)
+		if logDir == "" {
+			logDir = "."
+		}
+		os.MkdirAll(logDir, 0755)
+		logf, err = os.Create(o.Log)
+		if err != nil {
+			return err
+		}
+		cmd.Stdout = logf
+		cmd.Stderr = logf
+	}
+	runErr := cmd.Run()
+	var closeErr error
+	if logf != nil {
+		closeErr = logf.Close()
+	}
+	if runErr != nil {
+		if ctx.Err() != nil {
+			return fmt.Errorf("converter canceled: %w", ctx.Err())
+		}
+		return fmt.Errorf("converter failed: %w", runErr)
+	}
+	if closeErr != nil {
+		return closeErr
+	}
+	info, err := os.Stat(tmpPath)
+	if err != nil {
+		return fmt.Errorf("converter did not create output: %w", err)
+	}
+	if info.Size() == 0 {
+		return fmt.Errorf("converter created empty output")
+	}
+	if err := os.Rename(tmpPath, o.Output); err != nil {
+		return err
+	}
+	return nil
+}
